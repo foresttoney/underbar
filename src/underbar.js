743f6a4d1073f3542ab5298c4ae4ce811ceabe8a
foresttoney <foresttoney@gmail.com>
2014-04-23 00:11:55 -0700

    complete filter

--- src/underbar.js
+++ src/underbar.js
@@ -86,6 +86,13 @@ var _ = {};
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var results = [];
+    _.each(collection, function(value) {
+      if(test(value)) {
+        results.push(value);
+      }
+    });
+    return results; 
   };
 
   // Return all elements of an array that don't pass a truth test.
