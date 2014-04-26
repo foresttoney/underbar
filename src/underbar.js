c81527f64687b5dca3f50a8778c4b39552722b68
foresttoney <foresttoney@gmail.com>
2014-04-26 10:41:28 -0700

    complete reduce

--- src/underbar.js
+++ src/underbar.js
@@ -172,6 +172,10 @@ var _ = {};
   //     return total + number;
   //   }, 0); // should be 6
   _.reduce = function(collection, iterator, accumulator) {
+    _.each(collection, function(value) {
+      accumulator = iterator(value, accumulator);
+    }, accumulator);
+    return accumulator; 
   };
 
   // Determine if the array or object contains a given value (using `===`).
